3405efb79823231983c7268683e1e09ac5908ad0
Jason D Spooner <JasonDSpooner@gmail.com>
2026-04-08 16:43:49 -0600

    Update for production: remove localhost references

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ['10.0.0.116', 'localhost'],
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
